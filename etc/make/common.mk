bd688debc3a45505e3c62726604770e57ba24917
Bahman Movaqar <Bahman@BahmanM.com>
2026-07-16 22:38:36 -0700

    Upload the Python wheel

--- etc/make/common.mk
+++ etc/make/common.mk
@@ -13,6 +13,7 @@ LIBSCID_CTEST ?= ctest
 LIBSCID_UV ?= uv
 LIBSCID_TOX ?= tox
 LIBSCID_PYTHON ?= $(shell command -v python3 2>/dev/null || command -v python 2>/dev/null || echo python)
+LIBSCID_PLANTUML_JAR_PATH ?=
 
 LIBSCID_CMAKE_BUILD_TYPE ?= Release
 LIBSCID_CMAKE_BUILD_ARGS ?=
@@ -31,6 +32,7 @@ libscid.cmake.generator.arg := $(if $(LIBSCID_CMAKE_GENERATOR),-G "$(LIBSCID_CMA
 libscid.cmake.shared.libs.arg := $(if $(LIBSCID_CMAKE_SHARED_LIBS),-DBUILD_SHARED_LIBS=$(LIBSCID_CMAKE_SHARED_LIBS))
 libscid.cmake.c.compiler.arg := $(if $(LIBSCID_CMAKE_C_COMPILER),"-DCMAKE_C_COMPILER=$(LIBSCID_CMAKE_C_COMPILER)")
 libscid.cmake.cxx.compiler.arg := $(if $(LIBSCID_CMAKE_CXX_COMPILER),"-DCMAKE_CXX_COMPILER=$(LIBSCID_CMAKE_CXX_COMPILER)")
+libscid.cmake.plantuml.jar.path.arg := $(if $(LIBSCID_PLANTUML_JAR_PATH),"-DLIBSCID_PLANTUML_JAR_PATH=$(LIBSCID_PLANTUML_JAR_PATH)")
 
 ####################################################################################################
 
