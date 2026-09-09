17d6fa2f9d645f1422c4edf3bf8e7e28d3cbf028
Bahman Movaqar <Bahman@BahmanM.com>
2026-09-09 10:49:15 -0700

    Make linkage an explicit build concept

--- etc/make/cmake.mk
+++ etc/make/cmake.mk
@@ -12,34 +12,39 @@ __libscid_etc_make_cmake.mk := 1
 #
 # BASE_PROJECT
 #   Optional source project used for defaults.  This is useful for project variants such as
-#   `libscid.capi.default`, which should generate its own targets but inherit source-directory and
-#   default build-directory conventions from `libscid.capi`.
+#   `libscid.capi.static`, which should generate its own targets but inherit source-directory,
+#   build-targets, and test-labels from `libscid.capi`.
 #
 # Required project variables:
-#   PROJECT.__cmake.build.targets
+#   PROJECT.__cmake.build.targets (inherited from BASE_PROJECT if omitted)
 #
 # Optional project variables with defaults:
 #   PROJECT.__build.dir                      BASE_PROJECT.__project.dir + _build/
 #   PROJECT.__cmake.source.root              $(ROOT)
 #   PROJECT.__cmake.source.dir               BASE_PROJECT.__project.dir
-#   PROJECT.__cmake.shared.libs              $(LIBSCID_CMAKE_SHARED_LIBS)
+#   PROJECT.__cmake.linkage                  BASE_PROJECT.__cmake.linkage or $(LIBSCID_LINKAGE_TYPE)
 #   PROJECT.__cmake.install                  OFF
 #   PROJECT.__qc.format.build.dir            BASE_PROJECT.__project.dir + _build/format/
 #   PROJECT.__qc.analysis.build.dir          BASE_PROJECT.__project.dir + _build/analysis/
 #   PROJECT.__qc.dynamic-analysis.build.dir  BASE_PROJECT.__project.dir + _build/sanitisers/
 #
 # Optional project variables without defaults:
-#   PROJECT.__cmake.test.labels
+#   PROJECT.__cmake.test.labels              (inherited from BASE_PROJECT if omitted)
 ####################################################################################################
 
 define libscid.cmake.__project.rules
 ####################################################################################################
 
+$(1).__cmake.build.targets ?= $$($(2).__cmake.build.targets)
+$(1).__cmake.test.labels ?= $$($(2).__cmake.test.labels)
+$(1).__cmake.linkage ?= $$($(2).__cmake.linkage)
+
 $(1).__cmake.contract : \
   bmakelib.default-if-blank( $(1).__build.dir,$$(call libscid.__make.word.escape,$$($(or $(2),$(1)).__project.dir)_build/) ) \
   bmakelib.default-if-blank( $(1).__cmake.source.root,$$(call libscid.__make.word.escape,$$(ROOT)) ) \
   bmakelib.default-if-blank( $(1).__cmake.source.dir,$$(call libscid.__make.word.escape,$$($(or $(2),$(1)).__project.dir)) ) \
-  bmakelib.default-if-blank( $(1).__cmake.shared.libs,$$(LIBSCID_CMAKE_SHARED_LIBS) ) \
+  bmakelib.default-if-blank( $(1).__cmake.linkage,$$(LIBSCID_LINKAGE_TYPE) ) \
+  bmakelib.enum.error-unless-member( LIBSCID_LINKAGE_TYPE,$(1).__cmake.linkage ) \
   bmakelib.default-if-blank( $(1).__cmake.install,OFF ) \
   bmakelib.default-if-blank( $(1).__qc.format.build.dir,$$(call libscid.__make.word.escape,$$($(or $(2),$(1)).__project.dir)_build/format/) ) \
   bmakelib.default-if-blank( $(1).__qc.analysis.build.dir,$$(call libscid.__make.word.escape,$$($(or $(2),$(1)).__project.dir)_build/analysis/) ) \
@@ -50,7 +55,7 @@ $(1).__cmake.contract : \
     $(1).__cmake.source.root \
     $(1).__cmake.source.dir \
     $(1).__cmake.build.targets \
-    $(1).__cmake.shared.libs \
+    $(1).__cmake.linkage \
     $(1).__cmake.install \
     $(1).__qc.format.build.dir \
     $(1).__qc.analysis.build.dir \
@@ -67,7 +72,7 @@ $(1).configure : $(1).__cmake.contract
 	    -B $$($(1).__build.dir) \
 	    $$(libscid.cmake.__generator.arg) \
 	    -DBUILD_TESTING=ON \
-	    $$(if $$($(1).__cmake.shared.libs),-DBUILD_SHARED_LIBS=$$($(1).__cmake.shared.libs)) \
+	    -DBUILD_SHARED_LIBS=$$(if $$(filter static,$$($(1).__cmake.linkage)),OFF,ON) \
 	    -DLIBSCID_INSTALL=$$($(1).__cmake.install) \
 	    "-DLIBSCID_SOURCE_ROOT=$$($(1).__cmake.source.root)" \
 	    -DCMAKE_BUILD_TYPE=$$(LIBSCID_CMAKE_BUILD_TYPE) \
