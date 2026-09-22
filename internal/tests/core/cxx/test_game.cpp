70b3c2e8b39bb6adfd3c768cb3e477db3c45ef74
Bahman Movaqar <Bahman@BahmanM.com>
2026-09-22 15:30:21 -0700

    Tighten the tests

--- internal/tests/core/cxx/test_game.cpp
+++ internal/tests/core/cxx/test_game.cpp
@@ -278,4 +278,193 @@ namespace
         EXPECT_TRUE(savedMove.childVariations.empty());
     }
 
+    TEST(
+        CoreGameTest,
+        GameHeaderClearResetsAllFields)
+    {
+        scid::core::GameHeader header;
+        header.event.name = "Candidates";
+        header.event.site = "Toronto";
+        header.event.round = "5";
+        header.event.date = scid::core::date_parsePGNTag("2024.06.14", 10);
+        header.event.eventDate = scid::core::date_parsePGNTag("2024.06.01", 10);
+        header.white.name = "Kasparov";
+        header.white.rating = {2800, scid::core::RATING_Elo};
+        header.black.name = "Karpov";
+        header.black.rating = {2750, scid::core::RATING_Elo};
+        header.result = scid::core::RESULT_White;
+        header.eco = "B12";
+        header.tags.emplace_back("Annotator", "Deep Blue");
+
+        header.clear();
+
+        EXPECT_TRUE(header.event.name.empty());
+        EXPECT_TRUE(header.event.site.empty());
+        EXPECT_TRUE(header.event.round.empty());
+        EXPECT_EQ(scid::core::ZERO_DATE, header.event.date);
+        EXPECT_EQ(scid::core::ZERO_DATE, header.event.eventDate);
+        EXPECT_TRUE(header.white.name.empty());
+        EXPECT_EQ(0, header.white.rating.value);
+        EXPECT_TRUE(header.black.name.empty());
+        EXPECT_EQ(0, header.black.rating.value);
+        EXPECT_EQ(scid::core::RESULT_None, header.result);
+        EXPECT_TRUE(header.eco.empty());
+        EXPECT_TRUE(header.tags.empty());
+    }
+
+    TEST(
+        CoreGameTest,
+        GameHeaderSwapExchangesState)
+    {
+        scid::core::GameHeader h1;
+        h1.event.name = "Event 1";
+        h1.event.site = "Site 1";
+        h1.white.name = "Player 1";
+        h1.white.rating = {2600, scid::core::RATING_Elo};
+        h1.result = scid::core::RESULT_White;
+        h1.eco = "A01";
+        h1.tags.emplace_back("Key1", "Val1");
+
+        scid::core::GameHeader h2;
+        h2.event.name = "Event 2";
+        h2.event.site = "Site 2";
+        h2.white.name = "Player 2";
+        h2.white.rating = {2700, scid::core::RATING_Rapid};
+        h2.result = scid::core::RESULT_Black;
+        h2.eco = "B02";
+        h2.tags.emplace_back("Key2", "Val2");
+
+        h1.swap(h2);
+
+        EXPECT_EQ("Event 2", h1.event.name);
+        EXPECT_EQ("Site 2", h1.event.site);
+        EXPECT_EQ("Player 2", h1.white.name);
+        EXPECT_EQ(2700, h1.white.rating.value);
+        EXPECT_EQ(scid::core::RATING_Rapid, h1.white.rating.type);
+        EXPECT_EQ(scid::core::RESULT_Black, h1.result);
+        EXPECT_EQ("B02", h1.eco);
+        ASSERT_EQ(1U, h1.tags.size());
+        EXPECT_EQ("Key2", h1.tags[0].first);
+
+        EXPECT_EQ("Event 1", h2.event.name);
+        EXPECT_EQ("Site 1", h2.event.site);
+        EXPECT_EQ("Player 1", h2.white.name);
+        EXPECT_EQ(2600, h2.white.rating.value);
+        EXPECT_EQ(scid::core::RATING_Elo, h2.white.rating.type);
+        EXPECT_EQ(scid::core::RESULT_White, h2.result);
+        EXPECT_EQ("A01", h2.eco);
+        ASSERT_EQ(1U, h2.tags.size());
+        EXPECT_EQ("Key1", h2.tags[0].first);
+
+        using std::swap;
+        swap(h1, h2);
+
+        EXPECT_EQ("Event 1", h1.event.name);
+        EXPECT_EQ("Event 2", h2.event.name);
+    }
+
+    TEST(
+        CoreGameTest,
+        MovetextClearAndSwap)
+    {
+        scid::core::Movetext m1;
+        m1.initialComment = "Intro comment";
+        m1.mainline.appendMove({scid::core::E2, scid::core::E4, scid::core::EMPTY});
+
+        EXPECT_FALSE(m1.initialComment.empty());
+        EXPECT_EQ(1U, m1.mainline.moves.size());
+
+        m1.clear();
+
+        EXPECT_TRUE(m1.initialComment.empty());
+        EXPECT_TRUE(m1.mainline.moves.empty());
+
+        m1.initialComment = "Line A";
+        m1.mainline.appendMove({scid::core::E2, scid::core::E4, scid::core::EMPTY});
+
+        scid::core::Movetext m2;
+        m2.initialComment = "Line B";
+        m2.mainline.appendMove({scid::core::D2, scid::core::D4, scid::core::EMPTY});
+
+        m1.swap(m2);
+
+        EXPECT_EQ("Line B", m1.initialComment);
+        ASSERT_EQ(1U, m1.mainline.moves.size());
+        EXPECT_EQ(scid::core::D2, m1.mainline.moves[0].spec.from);
+
+        EXPECT_EQ("Line A", m2.initialComment);
+        ASSERT_EQ(1U, m2.mainline.moves.size());
+        EXPECT_EQ(scid::core::E2, m2.mainline.moves[0].spec.from);
+
+        using std::swap;
+        swap(m1, m2);
+
+        EXPECT_EQ("Line A", m1.initialComment);
+        EXPECT_EQ("Line B", m2.initialComment);
+    }
+
+    TEST(
+        CoreGameTest,
+        GameClearResetsAllSubObjects)
+    {
+        scid::core::Game game;
+        game.setEvent("World Championship");
+        game.setWhiteName("Capablanca");
+        ASSERT_EQ(scid::core::OK, game.setStartFen("8/K7/8/8/7k/8/8/8 w - - 45 25"));
+        game.setInitialComment("Game comment");
+        game.appendMainlineMove({scid::core::E2, scid::core::E4, scid::core::EMPTY});
+
+        EXPECT_FALSE(game.event().empty());
+        EXPECT_TRUE(game.hasNonStandardStart());
+        EXPECT_NE(nullptr, game.startPosition());
+        EXPECT_EQ(1U, game.mainlineHalfMoveCount());
+
+        game.clear();
+
+        EXPECT_TRUE(game.event().empty());
+        EXPECT_TRUE(game.white().name.empty());
+        EXPECT_FALSE(game.hasNonStandardStart());
+        EXPECT_EQ(nullptr, game.startPosition());
+        EXPECT_TRUE(game.initialComment().empty());
+        EXPECT_EQ(0U, game.mainlineHalfMoveCount());
+    }
+
+    TEST(
+        CoreGameTest,
+        GameSwapExchangesFullState)
+    {
+        scid::core::Game game1;
+        game1.setEvent("London 1851");
+        game1.setWhiteName("Anderssen");
+        game1.appendMainlineMove({scid::core::E2, scid::core::E4, scid::core::EMPTY});
+
+        scid::core::Game game2;
+        game2.setEvent("Reykjavik 1972");
+        game2.setWhiteName("Fischer");
+        ASSERT_EQ(scid::core::OK, game2.setStartFen("8/K7/8/8/7k/8/8/8 w - - 45 25"));
+        game2.appendMainlineMove({scid::core::D2, scid::core::D4, scid::core::EMPTY});
+
+        game1.swap(game2);
+
+        EXPECT_EQ("Reykjavik 1972", game1.event());
+        EXPECT_EQ("Fischer", game1.white().name);
+        EXPECT_TRUE(game1.hasNonStandardStart());
+        ASSERT_NE(nullptr, game1.startPosition());
+        ASSERT_EQ(1U, game1.mainlineHalfMoveCount());
+        EXPECT_EQ(scid::core::D2, game1.movetext().mainline.moves[0].spec.from);
+
+        EXPECT_EQ("London 1851", game2.event());
+        EXPECT_EQ("Anderssen", game2.white().name);
+        EXPECT_FALSE(game2.hasNonStandardStart());
+        EXPECT_EQ(nullptr, game2.startPosition());
+        ASSERT_EQ(1U, game2.mainlineHalfMoveCount());
+        EXPECT_EQ(scid::core::E2, game2.movetext().mainline.moves[0].spec.from);
+
+        using std::swap;
+        swap(game1, game2);
+
+        EXPECT_EQ("London 1851", game1.event());
+        EXPECT_EQ("Reykjavik 1972", game2.event());
+    }
+
 } // namespace
